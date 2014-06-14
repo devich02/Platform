ba296c296eda28dd07adf9e5caa74c62c580b0a9
Giancarlo Devich <devich02@gmail.com>
2014-06-14 12:54:15 -0700

    Added game object and platform

--- Platform/Platform/Form1.cs
+++ Platform/Platform/Form1.cs
@@ -16,5 +16,19 @@ namespace Platform
         {
             InitializeComponent();
         }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            List<GameObject> objs = new List<GameObject>() { new Platform(20,20,400,40), new Platform(20,200,400,40), new Scenery(400,400,20,20,Color.Green) };
+
+            foreach (GameObject g in objs)
+            {
+                g.draw(e.Graphics);
+            }
+
+            this.Invalidate();
+        }
+
     }
 }
---
+++ Platform/Platform/GameClasses.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Platform
+{
+
+    class GameObject
+    {
+        public float x = 0, 
+                     y = 0, 
+                     width = 0, 
+                     height = 0;
+        public virtual void update() { }
+        public virtual void draw(Graphics g) { }
+    }
+
+    class Platform : GameObject
+    {
+        public Color cPlatformColor;
+        public Platform() : this(10, 10, 100, 100) {}
+        public Platform(float x, float y, float width, float height) : this(x, y, width, height, Color.Black) {}
+        public Platform(float x, float y, float width, float height, Color color)
+        {
+            cPlatformColor = color;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(cPlatformColor), x, y, width, height);
+        }
+    }
+
+    class Scenery : GameObject
+    {
+        public Color cPlatformColor;
+        public Scenery() : this(10, 10, 100, 100) {}
+        public Scenery(float x, float y, float width, float height) : this(x, y, width, height, Color.Black) {}
+        public Scenery(float x, float y, float width, float height, Color color)
+        {
+            cPlatformColor = color;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(cPlatformColor), x, y, width, height);
+        }
+    }
+
+}
