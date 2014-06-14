eab4ac1737402c2115b8b428c4f3ef99ba489cc3
Giancarlo Devich <devich02@gmail.com>
2014-06-14 18:48:42 -0500

    Added moving platforms, main character, gravity

--- Platform/Platform/Form1.cs
+++ Platform/Platform/Form1.cs
@@ -12,22 +12,63 @@ namespace Platform
 {
     public partial class Form1 : Form
     {
+        float cameraX = -100,
+            cameraY = -400;
+        bool[] keys; 
+
         public Form1()
         {
             InitializeComponent();
-        }
+            keys = new bool[Enum.GetValues(typeof(Keys)).Length];
+            MC.x = ClientRectangle.Width / 2 - MC.width / 2;
+            MC.y = ClientRectangle.Height / 2 - MC.height / 2;
+            MC.ay = .001f;
 
+        }
+    List<GameObject> objs = new List<GameObject>() { new MovingPlatform(20,20,100,40,Color.Black ,1,0,0,0,500,0), new Platform(20,200,400,40), new Scenery(400,400,20,20,Color.Green) };
+    MainCharacter MC = new MainCharacter(10, 10, 20, 60);
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-
-            List<GameObject> objs = new List<GameObject>() { new Platform(20,20,400,40), new Platform(20,200,400,40), new Scenery(400,400,20,20,Color.Green) };
+            if (keys[(int)Keys.Up])
+            {
+                cameraY -= 1;
+            }
+            if (keys[(int)Keys.Down])
+            {
+                cameraY += 1;
+            }
+            if (keys[(int)Keys.Left])
+            {
+                cameraX -= 1;
+            }
+            if (keys[(int)Keys.Right])
+            {
+                cameraX += 1;
+            }
 
             foreach (GameObject g in objs)
             {
+                g.update(cameraX, cameraY, objs, e.Graphics);
                 g.draw(e.Graphics);
             }
+            cameraX += MC.vx;
+            cameraY += MC.vy;
+            MC.vx += MC.ax;
+            MC.vy += MC.ay;
+            MC.update(cameraX, cameraY, objs, e.Graphics);
+            MC.draw(e.Graphics);
 
             this.Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            keys[(int)e.KeyCode] = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            keys[(int)e.KeyCode] = false;
         }
 
     }
--- Platform/Platform/GameClasses.cs
+++ Platform/Platform/GameClasses.cs
@@ -11,11 +11,28 @@ namespace Platform
 
     class GameObject
     {
-        public float x = 0, 
-                     y = 0, 
-                     width = 0, 
-                     height = 0;
-        public virtual void update() { }
+        public float x = 0,
+                     y = 0,
+                     offsetX = 0,
+                     offsetY = 0,
+                     width = 0,
+                     height = 0,
+                     vx = 0,
+                     vy =0,
+                     ax =0,
+                     ay = 0;
+
+        public virtual void update(float offsetX, float offsetY, List<GameObject> os, Graphics g)
+        {
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
+            x += vx;
+            y += vy;
+            vx += ax;
+            vy += ay;
+
+
+        }
         public virtual void draw(Graphics g) { }
     }
 
@@ -35,7 +52,46 @@ namespace Platform
 
         public override void draw(Graphics g)
         {
-            g.FillRectangle(new SolidBrush(cPlatformColor), x, y, width, height);
+            g.FillRectangle(new SolidBrush(cPlatformColor), x-offsetX, y-offsetY, width, height);
+        }
+    }
+
+    class MovingPlatform : Platform
+    {
+        float minX = 0,
+            minY = 0,
+            maxX = 0,
+            maxY = 0;
+
+        public MovingPlatform() : this(10, 10, 100, 100) { }
+        public MovingPlatform(float x, float y, float width, float height) : this(x, y, width, height, Color.Black) {}
+        public MovingPlatform(float x, float y, float width, float height, Color color) : this(x, y, width, height, color, 0, 0, 0, 0, 0, 0) { }
+        public MovingPlatform (float x, float y, float width, float height, Color color, float vx, float vy, float minX, float minY, float maxX, float maxY)
+        {
+            cPlatformColor = color;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.vx = vx;
+            this.vy = vy;
+            this.minX = minX;
+            this.minY = minY;
+            this.maxX = maxX;
+            this.maxY = maxY;
+        }
+
+        public override void update(float offsetX, float offsetY, List<GameObject> os, Graphics g)
+        {
+            base.update(offsetX, offsetY, os, g);
+            if (x + width >= maxX || x <= minX)
+            {
+                vx = -vx;
+            }
+            if (y + height >= maxY || y <= minY)
+            {
+                vy = -vy;
+            }
         }
     }
 
@@ -55,8 +111,46 @@ namespace Platform
 
         public override void draw(Graphics g)
         {
-            g.FillRectangle(new SolidBrush(cPlatformColor), x, y, width, height);
+            g.FillRectangle(new SolidBrush(cPlatformColor), x - offsetX, y - offsetY, width, height);
         }
     }
 
+    class Character : GameObject
+    {
+
+    }
+
+    class MainCharacter : Character
+    {
+        public MainCharacter(float x, float y, float width, float height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+        public override void draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Black), x,y, width, height);
+        }
+
+        public override void update(float offsetX, float offsetY, List<GameObject> os, Graphics g)
+        {
+            RectangleF me = new RectangleF(x, y, width, height);
+            g.DrawRectangle(Pens.Orange, me.X, me.Y, me.Width, me.Height);
+            ay = .0001f;
+            foreach (GameObject o in os)
+            {
+                RectangleF other = new RectangleF(o.x - offsetX,o.y - offsetY,o.width,o.height);
+                if (me.IntersectsWith(other))
+                {
+               
+                    ay = 0;
+                    vy = 0;
+                }
+                g.DrawRectangle(Pens.Orange, other.X, other.Y, other.Width, other.Height);
+            }
+
+        }
+    }
 }
